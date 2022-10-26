32841b2897f4b220202b6adb4b4d73991a9fdc52
DmitrySergeevich <efos.ds@gmail.com>
2022-10-26 19:03:12 +0700

    title data-attribute

--- src/components/elements/ColorWrapper/ColorWrapper.jsx
+++ src/components/elements/ColorWrapper/ColorWrapper.jsx
@@ -3,19 +3,21 @@ import Container from "../../templates/Container";
 import useMobileDetect from "use-mobile-detect-hook";
 
 import styles from "./ColorWrapper.module.scss";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 const ColorWrapper = ({ name, color, children }) => {
   const detectMobile = useMobileDetect();
   const isMobile = detectMobile.isMobile();
-  const [title, setTitle] = useState('');
-  const exhibitions = useSelector(state => state.exhibitions.exhibitions[0])
+  const [title, setTitle] = useState("");
+  const exhibitions = useSelector((state) => state.exhibitions.exhibitions[0]);
 
   useEffect(() => {
     if (exhibitions) {
-      setTitle(exhibitions.title)
+      setTitle(exhibitions.title);
     }
-  }, [exhibitions])
+  }, [exhibitions]);
+
+  // console.log(title);
 
   return (
     <div
@@ -25,10 +27,9 @@ const ColorWrapper = ({ name, color, children }) => {
           : { backgroundColor: color, paddingTop: 150, paddingBottom: 30 }
       }
       className={styles.colorWrapper}
+      data-title={title}
     >
-      <Container>
-        {children}
-      </Container>
+      <Container>{children}</Container>
     </div>
   );
 };
