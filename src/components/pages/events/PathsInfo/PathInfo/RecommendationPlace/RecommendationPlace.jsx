862d38ac0a16213e2e21ccd2170f52d8ed33215e
DmitrySergeevich <efos.ds@gmail.com>
2022-11-21 18:11:59 +0700

    PathInfo  changes

--- src/components/pages/events/PathsInfo/PathInfo/RecommendationPlace/RecommendationPlace.jsx
+++ src/components/pages/events/PathsInfo/PathInfo/RecommendationPlace/RecommendationPlace.jsx
@@ -5,8 +5,8 @@ const RecommendationPlace = ({ place }) => {
   return (
     <div className={styles.place}>
       <img src={place.image} alt="" className={styles.image} />
-      <p className={styles.location}>{place.location}</p>
       <p className={styles.title}>{place.title}</p>
+      <p className={styles.location}>{place.location}</p>
     </div>
   );
 };
