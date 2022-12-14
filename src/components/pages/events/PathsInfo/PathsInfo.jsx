c277718ec7126bb67200c8ba6e8546361490fa85
DmitrySergeevich <efos.ds@gmail.com>
2022-12-14 11:57:11 +0700

    fade animation in 'whereToGo' (have problem)

--- src/components/pages/events/PathsInfo/PathsInfo.jsx
+++ src/components/pages/events/PathsInfo/PathsInfo.jsx
@@ -1,17 +1,23 @@
-import React, { useEffect, useState } from "react";
-import PathInfo from "./PathInfo/PathInfo";
-import styles from "./PathsInfo.module.scss";
+import React, { useEffect, useState, useRef } from 'react';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import PathInfo from './PathInfo/PathInfo';
+import styles from './PathsInfo.module.scss';
 
-import useMobileDetect from "use-mobile-detect-hook";
+import useMobileDetect from 'use-mobile-detect-hook';
 
-const PathsInfo = ({ paths }) => {
+const PathsInfo = ({ paths, customRef }) => {
   const detectMobile = useMobileDetect();
   const isMobile = detectMobile.isMobile();
+
+  const mode = 'out-in';
+  const infoRef = useRef(null);
 
   let [activePath, setActivePath] = useState({});
   useEffect(() => {
     setActivePath(paths[0]);
   }, [paths]);
+
+  // console.log(activePath.id);
 
   return (
     <div
@@ -20,6 +26,7 @@ const PathsInfo = ({ paths }) => {
           ? `${styles.path_info} ${styles.path_info__mobile}`
           : styles.path_info
       }
+      ref={customRef}
     >
       <div className={styles.buttons}>
         {paths.map((path) => (
@@ -34,8 +41,19 @@ const PathsInfo = ({ paths }) => {
           </button>
         ))}
       </div>
-
-      {activePath && <PathInfo path={activePath} />}
+      <SwitchTransition mode={mode}>
+        <CSSTransition
+          className={'fade'}
+          key={activePath?.id}
+          nodeRef={infoRef}
+          timeout={{ enter: 250, exit: 250 }}
+          unmountOnExit
+        >
+          <div ref={infoRef}>
+            {activePath && <PathInfo path={activePath} />}
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   );
 };
