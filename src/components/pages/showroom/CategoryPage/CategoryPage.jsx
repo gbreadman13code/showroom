5a5b58754ecdf6dfedb908e23fa9d6940f3395c3
DmitrySergeevich <efos.ds@gmail.com>
2022-12-14 12:27:25 +0700

    fade animation CategoryPage in showroom

--- src/components/pages/showroom/CategoryPage/CategoryPage.jsx
+++ src/components/pages/showroom/CategoryPage/CategoryPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styles from './CategoryPage.module.scss';
 import { getShopsCategories } from '../../../../redux/requests/getShopsCategories';
 import { Link, useLocation, useParams, useNavigate } from 'react-router-dom';
@@ -9,7 +9,12 @@ import CategoryBlankPage from './CategoryBlankPage/CategoryBlankPage';
 import NavBar from '../../../elements/NavBar/NavBar';
 import CategoryCards from './CategoryCards/CategoryCards';
 
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
+
 const CategoryPage = ({ orderDict }) => {
+  const mode = 'out-in';
+  const cardRef = useRef(null);
+
   let params = useParams();
   let [categories, setCategories] = useState([]);
   let [activeCategory, setActiveCategory] = useState(0);
@@ -33,12 +38,35 @@ const CategoryPage = ({ orderDict }) => {
     navigate(`/showroom/categories/${category_id}`);
   };
 
+  // console.log(activeCategory);
+
   return (
     <div className={styles.wrapper}>
-      <PageTemplate header='absolute' orderLink='/showroom/order' order={true} orderDict={orderDict}>
+      <PageTemplate
+        header="absolute"
+        orderLink="/showroom/order"
+        order={true}
+        orderDict={orderDict}
+      >
         <Container>
-          <NavBar categories={categories} activeCategory={activeCategory} onClickCallback={changeActiveCategory} />
-          <CategoryCards activeCategory={activeCategory} shops={shops} />
+          <NavBar
+            categories={categories}
+            activeCategory={activeCategory}
+            onClickCallback={changeActiveCategory}
+          />
+          <SwitchTransition mode={mode}>
+            <CSSTransition
+              className={'fade'}
+              key={activeCategory}
+              nodeRef={cardRef}
+              timeout={{ enter: 250, exit: 250 }}
+              unmountOnExit
+            >
+              <div ref={cardRef}>
+                <CategoryCards activeCategory={activeCategory} shops={shops} />
+              </div>
+            </CSSTransition>
+          </SwitchTransition>
         </Container>
       </PageTemplate>
     </div>
