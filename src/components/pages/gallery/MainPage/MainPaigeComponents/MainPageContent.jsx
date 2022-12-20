5dc64c359ffd771aefe36323007d9f087c329507
DmitrySergeevich <efos.ds@gmail.com>
2022-12-20 10:33:01 +0700

    edits in Gallery MainContent

--- src/components/pages/gallery/MainPage/MainPaigeComponents/MainPageContent.jsx
+++ src/components/pages/gallery/MainPage/MainPaigeComponents/MainPageContent.jsx
@@ -5,14 +5,20 @@ import ButtonsBlot from '../../../../elements/ButtonsBlot/ButtonsBlot';
 import styles from '../MainPage.module.scss';
 import DescriptionSlider from './Slider/DescriptionSlider';
 
-const MainPageContent = ({ data, sliderContent, activeExhibition, setActiveExhibition, exhibitionsCount }) => {
+const MainPageContent = ({
+  data,
+  sliderContent,
+  activeExhibition,
+  setActiveExhibition,
+  exhibitionsCount,
+}) => {
   const { image, title, sub_title, description } = data;
   return (
     <div className={styles.main_page_container}>
       <div className={styles.image_wrapper}>
         <img src={image} alt={title} />
       </div>
-      <div>
+      <div className={styles.desc_wrapper}>
         <h1>{title}</h1>
         <h2>{sub_title}</h2>
         <div>
@@ -29,7 +35,10 @@ const MainPageContent = ({ data, sliderContent, activeExhibition, setActiveExhib
           />
         </div>
         <div className={styles.slider_wrapper}>
-          <DescriptionSlider description={description} sliderContent={sliderContent} />
+          <DescriptionSlider
+            description={description}
+            sliderContent={sliderContent}
+          />
         </div>
       </div>
     </div>
